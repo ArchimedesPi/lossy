86e310d25d3ce514d8a7e4c8e8a223c71073db04
ArchimedesPi <archimedespi3141@gmail.com>
2014-06-26 12:52:30 -0500

    Restructure and other stuff

--- kernel/arch/x86/vgadriver.h
+++ kernel/arch/x86/vgadriver.h
@@ -1,8 +1,26 @@
 #include <stddef.h>
 #include <stdint.h>
-#include "vga_colors.h"
 
 // Super simple graphics driver implementation
+
+enum vga_color {
+	COLOR_BLACK = 0,
+	COLOR_BLUE = 1,
+	COLOR_GREEN = 2,
+	COLOR_CYAN = 3,
+	COLOR_RED = 4,
+	COLOR_MAGENTA = 5,
+	COLOR_BROWN = 6,
+	COLOR_LIGHT_GREY = 7,
+	COLOR_DARK_GREY = 8,
+	COLOR_LIGHT_BLUE = 9,
+	COLOR_LIGHT_GREEN = 10,
+	COLOR_LIGHT_CYAN = 11,
+	COLOR_LIGHT_RED = 12,
+	COLOR_LIGHT_MAGENTA = 13,
+	COLOR_LIGHT_BROWN = 14,
+	COLOR_WHITE = 15,
+};
 
 class BIOSVGAterm {
 public:
