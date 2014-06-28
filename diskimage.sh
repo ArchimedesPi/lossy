144fa2cea01951b5f71b1e6bf0eb5ceb85a6c8ea
ArchimedesPi <archimedespi3141@gmail.com>
2014-06-27 19:50:17 -0500

    Scripts

--- diskimage.sh
+++ diskimage.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Written by Samy Pessé, modified by ArchimedesPi
 qemu-img create c.img 2M
 fdisk ./c.img << EOF
 x
@@ -23,15 +24,19 @@ losetup -o 32256 /dev/loop1 ./c.img
 
 mke2fs /dev/loop1
 mount /dev/loop1 /mnt/
+
 cp -R ./bootdisk/* /mnt/
-umount /mnt/
-grub --device-map=/dev/null << EOF
-device (hd0) ./c.img
-geometry (hd0) 4 16 63
-root (hd0,0)
-setup (hd0)
-quit
+mkdir -p /mnt/boot/grub
+cat > /mnt/boot/grub/device.map <<EOF
+(hd0) /dev/loop0
+(hd0,1) /dev/loop1
 EOF
+
+mount --bind /dev /mnt/dev
+chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
+
+umount /mnt/dev
+umount /mnt
 
 losetup -d /dev/loop1
 
