dee4aa82413a017ce4e16131074ce60a3e82077a
ArchimedesPi <archimedespi3141@gmail.com>
2014-07-15 11:09:17 -0500

    Add more string functions

--- kernel/runtime/string.h
+++ kernel/runtime/string.h
@@ -4,6 +4,6 @@
 
 extern "C" int strlen(char *s);
 extern "C" int strcmp(const char *dst, char *src);
-//extern "C" void strcat(void *dest,const void *src);
+extern "C" void strcat(void *dest,const void *src);
 
 #endif
