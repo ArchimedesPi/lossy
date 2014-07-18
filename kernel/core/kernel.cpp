b6e04ec3e306c718cb230f96b3d4cdb58ff8ef02
ArchimedesPi <archimedespi3141@gmail.com>
2014-07-18 18:25:02 -0500

    Add the license, remove junk

--- kernel/core/kernel.cpp
+++ kernel/core/kernel.cpp
@@ -6,12 +6,10 @@
 
 extern "C"
 void kmain() {
-	IO io = IO();
+	extern IO io = IO();
 	io.terminal.setColors(VGATerminal::Black, VGATerminal::LightGrey);
 	io.terminal.clear();
 	io.terminal.setCursor(0,0);
 	io.terminal.puts(KERNEL_NAME " - " KERNEL_VERSION " -- " KERNEL_DATE " " KERNEL_TIME "\n");
-	io.terminal.puts("This is a second line!\n");
-	io.terminal.putc('\n');
-	io.terminal.puts("And a third (actually fourth) line! Isn't this exciting?\n");
+	io.terminal.puts("Licensed under the " KERNEL_LICENSE "\n")
 }
