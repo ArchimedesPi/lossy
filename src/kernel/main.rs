cfc376ad95ef1702cceb5e3edff8bc02f2321c05
Liam Marshall <archimedespi3141@gmail.com>
2015-03-28 15:15:21 -0500

    More options and attributes to try and get this thing to compile

--- src/kernel/main.rs
+++ src/kernel/main.rs
@@ -1,8 +1,11 @@
-#![nostd]
+#![feature(no_std)]
+#![feature(asm)]
+#![feature(lang_items)]
+
+#![no_std]
 #![allow(ctypes)]
 
-#![no_mangle]
-#![no_split_stack]
+#[no_mangle]
 pub fn main() {
 	/* Do nothing here yet.
 	   This will make the `call main` in boot.asm exit and the execution will continue,
