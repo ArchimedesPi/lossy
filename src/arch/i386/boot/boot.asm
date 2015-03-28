5f5c621b9cda35bb553400d962303361798ec7ac
Liam Marshall <archimedespi3141@gmail.com>
2015-03-28 09:45:38 -0500

    Editor reindent

--- src/arch/i386/boot/boot.asm
+++ src/arch/i386/boot/boot.asm
@@ -4,7 +4,7 @@ MEMINFO     equ  1<<1                   ; provide memory map
 FLAGS       equ  MBALIGN | MEMINFO      ; this is the Multiboot 'flag' field
 MAGIC       equ  0x1BADB002             ; 'magic number' lets bootloader find the header
 CHECKSUM    equ -(MAGIC + FLAGS)        ; checksum of above, to prove we are multiboot
- 
+
 ; Declare a header as in the Multiboot Standard. We put this into a special
 ; section so we can force the header to be in the start of the final program.
 ; You don't need to understand all these details as it is just magic values that
@@ -15,7 +15,7 @@ align 4
    dd MAGIC
    dd FLAGS
    dd CHECKSUM
- 
+
 ; Currently the stack pointer register (esp) points at anything and using it may
 ; cause massive harm. Instead, we'll provide our own stack. We will allocate
 ; room for a small temporary stack by creating a symbol at the bottom of it,
@@ -25,7 +25,7 @@ align 4
 stack_bottom:
 times 16384 db 0
 stack_top:
- 
+
 ; The linker script specifies _start as the entry point to the kernel and the
 ; bootloader will jump to this position once the kernel has been loaded. It
 ; doesn't make sense to return from this function as the bootloader is gone.
@@ -36,14 +36,14 @@ _start:
    ; such code. Rust doesn't expect much at this point and we only need to set up
    ; a stack. Note that the processor is not fully initialized yet and stuff
    ; such as floating point instructions are not available yet.
- 
+
    ; To set up a stack, we simply set the esp register to point to the top of
    ; our stack (as it grows downwards).
    mov esp, stack_top
- 
+
    extern main
    call main
- 
+
    ; In case the function returns, we'll want to put the computer into an
    ; infinite loop. To do that, we use the clear interrupt ('cli') instruction
    ; to disable interrupts, the halt instruction ('hlt') to stop the CPU until
