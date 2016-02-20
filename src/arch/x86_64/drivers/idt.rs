4b2457ec86fcdab5018635b634647fa81e5cd0e3
Liam Marshall <liam@cpan.org>
2016-02-19 21:35:24 -0600

    Update IDT for long mode

--- src/arch/x86_64/drivers/idt.rs
+++ src/arch/x86_64/drivers/idt.rs
@@ -1,53 +1,81 @@
 use core::mem;
+use core::intrinsics::transmute;
 use arch::x86_64::drivers::pic;
 
 const IDTSIZE: usize = 256;
 
 pub static mut IDT: [IDTEntry; IDTSIZE] = 
-    [IDTEntry {base_lo: 0, base_hi: 0, select: 0, flags: 0,}; IDTSIZE];
-pub static mut IDT_POINTER: IDTPointer = IDTPointer {base: 0, limit: 0};
+    [IDTEntry {offset_lo: 0, offset_mid: 0, offset_hi:0,
+     select: 0, flags: 0,
+     zero: 0, zero_: 0}; IDTSIZE];
+pub static mut IDT_POINTER: IDTPointer = IDTPointer {offset: 0, limit: 0};
 
 #[repr(C, packed)]
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug)]
 pub struct IDTPointer {
     limit: u16,
-    base: u32,
+    offset: u64,
 }
 
 #[repr(C, packed)]
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug)]
 pub struct IDTEntry {
-    base_lo: u16,
+    offset_lo: u16,
     select: u16,
+    zero: u8,
     flags: u8,
-    base_hi: u16,
+    offset_mid: u16,
+    offset_hi: u32,
+    zero_: u8,
 }
 
 impl IDTEntry {
     pub fn set(&mut self, isr: unsafe extern fn(), select: u16, flags: u8) {
-        let base = isr as u32;
-        self.base_lo = (base & 0xffff) as u16;
-        self.base_hi = ((base >> 16) & 0xffff) as u16;
+        let offset = isr as u64;
+        println!("isr offset: {:x}", offset);
+
+        self.offset_lo = (offset & 0xffff) as u16;
+        println!("isr offset low: {:x}", self.offset_lo);
+        self.offset_mid = ((offset >> 16) & 0xffff) as u16;
+        println!("isr offset mid: {:x}", self.offset_mid);
+        self.offset_hi = (offset >> 32) as u32;
+        println!("isr offset high: {:x}", self.offset_hi);
         self.select = select;
         self.flags = flags;
     }
 }
 
-extern {
-    fn isr_wrapper_kbd();
+unsafe fn lidt(idt: &IDTPointer) {
+    println!("lidt {:?}", *idt);
+    asm!("lidt ($0)" :: "r"(idt));
+}
+
+unsafe fn enable() {
+    asm!("sti");
+}
+
+unsafe fn disable() {
+    asm!("cli");
+}
+
+unsafe extern fn key_hit() {
+    asm!("iretq");
 }
 
 pub unsafe fn install_idt() {
-    IDT_POINTER.limit = ((mem::size_of::<IDTEntry>() * IDTSIZE) - 1) as u16;
-    IDT_POINTER.base = &IDT as *const [IDTEntry; IDTSIZE] as u32;
+    IDT_POINTER = IDTPointer {
+        limit: ((mem::size_of::<IDTEntry>() * IDTSIZE) - 1) as u16,
+        offset: transmute::<&[IDTEntry; IDTSIZE], u64>(&IDT),
+    };
 
-    IDT[33].set(isr_wrapper_kbd, 0x08, 0x8E);
+    IDT[33].set(key_hit, 0x08, 0x8E);
 
     pic::PICS.lock().initialize();
 
     pic::PICS.lock().mask_all_irqs();
-    pic::PICS.lock().unmask_irq(1); // unmask the keyboard irq
+    pic::PICS.lock().unmask_irq(1);
 
-    asm!("lidt ($0)" :: "r" (IDT));
-    asm!("sti");
+    lidt(&IDT_POINTER);
+
+    enable();
 }
--- src/arch/x86_64/isr_wrappers.asm
+++
@@ -1,9 +0,0 @@
-global isr_wrapper_kbd
-
-section .text
-bits 64
-align 8
-isr_wrapper_kbd:
-    extern _isr_kbd_handler
-    call _isr_kbd_handler
-    iretq
