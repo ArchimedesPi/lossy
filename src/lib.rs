4e1e15dfb2c71f9551315a53be4285f5460791cd
Liam Marshall <liam@cpan.org>
2016-02-21 21:59:09 -0600

    Wire in memory test

--- src/lib.rs
+++ src/lib.rs
@@ -54,18 +54,7 @@ pub extern fn kernel_main(multiboot_header_address: usize) {
     let mut frame_allocator = memory::AreaFrameAllocator::new(
         kernel_start as usize, kernel_end as usize, multiboot_start,
         multiboot_end, memory_map_tag.memory_areas());
-
-    for i in 0.. {
-        use memory::FrameAllocator;
-        if let None = frame_allocator.allocate_frame() {
-            println!("allocated {} frames", i);
-            break;
-        }
-    }
-
-    unsafe {
-        idt::install_idt();
-    }
+    memory::test_paging(&mut frame_allocator);
 
     loop {}
 }
